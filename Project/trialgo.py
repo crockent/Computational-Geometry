284c6a21a2178ff23c30f333bbf3001013717624
gkol <kosmas.spyridakis3@gmail.com>
2025-06-13 16:00:35 +0300

    skata me fraoules

--- Project/trialgo.py
+++ Project/trialgo.py
@@ -4,11 +4,21 @@ from dceltest import build_irregular_decagon_dcel
 
 
 
+def print_first_and_last_vertex_by_x(self):
+        if not self.vertices:
+            return None
+        sorted_vertices = sorted(self.vertices, key=lambda v: v.point.x)
+        last = sorted_vertices[-1].point
+        first = sorted_vertices[0].point
+        
+        
+        return first, last 
 
 
-def triangulation(dcel):
-  """ This function would implement the triangulation algorithm
-    1. pio dexia kai aristera simeia 
+
+""" This function would implement the triangulation algorithm
+    1. pio dexia kai aristera simeia ( tik )
+    
     1.5 koxe poligono stin mesi 
     2. 2 listes pano simia kato simia 
     3. sort os prox to x ola mazi 
@@ -34,6 +44,13 @@ def triangulation(dcel):
 
 
 
+
+
+
+def triangulation(dcel):
+    last,first = print_first_and_last_vertex_by_x(dcel) 
+    print(f"First vertex by x: ({first.x}, {first.y})")
+    print(f"Last vertex by x: ({last.x}, {last.y})")
 
 
 
